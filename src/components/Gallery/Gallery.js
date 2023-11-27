60b3034e27108050a23a0e3e937f5ec654ab2a2e
Clem <cmonmarche.contact@gmail.com>
2023-11-27 11:36:31 +0100

    separate like component and display only when image is loaded

--- src/components/Gallery/Gallery.js
+++ src/components/Gallery/Gallery.js
@@ -1,7 +1,22 @@
+import React from 'react';
 import LikedBtn from './LikedBtn';
 
 export default function Gallery({imagesToDisplay, addLikedImage}) {
 
+  const [likesBtn, setLikesBtn] = React.useState({})
+
+  const addLikedBtn = (image) => {
+    setLikesBtn(prevState => ({
+      ...prevState,
+      [image.key]: (
+        <LikedBtn
+          key={image.key}
+          onClick={() => addLikedImage(prevState => [...prevState, image])}
+        />
+      ),
+    }))
+  }
+ 
   return (
     <div className="lg:columns-4 sm:columns-2 ml-5 mr-5">
       {
@@ -11,10 +26,10 @@ export default function Gallery({imagesToDisplay, addLikedImage}) {
           key={image.key}>
             <img 
               className="rounded-lg" 
-              src={image.url}/>
-            <LikedBtn
-              onClick={() => addLikedImage(prevState => [...prevState, image])}
+              src={image.url}
+              onLoad={() => addLikedBtn(image)}
             />
+            {likesBtn[image.key]}
         </div>)
       }
     </div>
